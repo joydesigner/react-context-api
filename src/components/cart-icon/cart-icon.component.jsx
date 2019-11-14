e50405ee38723db56c8f53c9a26263a0609693b2
Xin Zheng <jason.zhengxin@gmail.com>
2019-11-14 18:01:28 +1100

    react context example project

---
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,30 @@
+import React, { useContext } from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
+
+import CartContext from '../../contexts/cart/cart.context';
+
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+
+import './cart-icon.styles.scss';
+
+const CartIcon = ({ itemCount }) => {
+  const { toggleHidden } = useContext(CartContext);
+  return (
+    <div className='cart-icon' onClick={toggleHidden}>
+      <ShoppingIcon className='shopping-icon' />
+      <span className='item-count'>{itemCount}</span>
+    </div>
+  )
+};
+
+
+const mapStateToProps = createStructuredSelector({
+  itemCount: selectCartItemsCount
+});
+
+export default connect(
+  mapStateToProps
+)(CartIcon);
